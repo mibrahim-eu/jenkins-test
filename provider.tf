18a752c892f351d176309c6e226975b2346ece88
Mahmoud Ibrahim <101556169+mibrahim-eu@users.noreply.github.com>
2023-10-27 19:19:32 +0200

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -7,10 +7,11 @@ terraform {
   }
 }
 
-
+/*
 # Configure the AWS Provider
 provider "aws" {
   access_key = var.aws_access_key
   secret_key = var.aws_secret_key
   region     = var.aws_region
 }
+*/
